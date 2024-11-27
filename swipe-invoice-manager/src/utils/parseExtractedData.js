3d5f4389f12d324895bfda61e07de56af8eb0b18
Shweta <143203479+shwet46@users.noreply.github.com>
2024-11-27 14:40:06 +0530

    .

--- swipe-invoice-manager/src/utils/parseExtractedData.js
+++
@@ -1,9 +0,0 @@
-const parseExtractedData = (rawData) => {
-    const invoices = rawData.invoices || [];
-    const products = rawData.products || [];
-    const customers = rawData.customers || [];
-
-    return { invoices, products, customers };
-};
-
-export default parseExtractedData;
